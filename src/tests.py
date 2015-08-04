adf1dc3bb027dde9ed64c75c0629a76b18259387
Amir Rachum <amir@rachum.com>
2015-08-04 23:20:05 +0300

    More tests!

--- src/tests.py
+++ src/tests.py
@@ -24,6 +24,16 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(f.x, 2)
         self.assertEqual(f.y, 'blah')
 
+    def test_attribute_access_missing_values(self):
+        f = Foo(2)
+        self.assertEqual(f.x, 2)
+        self.assertEqual(f.y, None)
+
+    def test_attribute_access_missing_values_partial_kwargs(self):
+        f = Foo(y=2)
+        self.assertEqual(f.x, None)
+        self.assertEqual(f.y, 2)
+
     def test_comparisons(self):
         small = Foo(1, 'irreleant')
         medium = Foo(2, 5)
@@ -47,6 +57,11 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(large > medium)
         self.assertTrue(medium >= another_medium)
         self.assertTrue(another_medium >= medium)
+
+        self.assertTrue(small != medium)
+        self.assertTrue(medium != small)
+        self.assertTrue(medium != large)
+        self.assertTrue(large != medium)
 
     def test_repr(self):
         f = Foo(1, 'irrelevant')
